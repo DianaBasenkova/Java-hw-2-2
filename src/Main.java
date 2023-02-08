17ca07b2a358832ec092d51a0abc6b2c7cd3d2c4
Diana Basenkova <dinnight123@yandex.ru>
2023-02-08 15:14:15 +0300

    Add comments

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,8 @@ public class Main {
         int IncomingPayment = 1800; // сумма пополнения
         int payment = 1000; // сумма при превышении которой начисляются бонусы
         int bonus = IncomingPayment>payment ? ((IncomingPayment-payment) / 100) : (0); // если платеж >1000, то рассчитать бонус с разницы превышающей суммы;
-        System.out.println(bonus);
+        System.out.println(bonus + " бонусных рублей");
         int BalanceAfter = BalanceBefore+IncomingPayment+bonus; // рассчитать итоговый баланс (баланс до пополнения+ сумма пополнения + бонус)
-        System.out.println(BalanceAfter);
+        System.out.println(BalanceAfter + " итоговый балланс");
     }
 }
