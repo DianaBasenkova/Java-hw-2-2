f42c3f7ea8a865ae792e3544dba7cd4e2edb3913
Diana Basenkova <dinnight123@yandex.ru>
2023-02-09 16:21:54 +0300

    fixed register

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args) {
 
-        int BalanceBefore = 85; //баланс до пополнения
-        int IncomingPayment = 1800; // сумма пополнения
+        int balanceBefore = 85; //баланс до пополнения
+        int incomingPayment = 900; // сумма пополнения
         int payment = 1000; // сумма при превышении которой начисляются бонусы
-        int bonus = IncomingPayment>payment ? ((IncomingPayment-payment) / 100) : (0); // если платеж >1000, то рассчитать бонус с разницы превышающей суммы;
+        int bonus = incomingPayment>payment ? ((incomingPayment - payment) / 100) : (0); // если платеж >1000, то рассчитать бонус с разницы превышающей суммы;
         System.out.println(bonus + " бонусных рублей");
-        int BalanceAfter = BalanceBefore+IncomingPayment+bonus; // рассчитать итоговый баланс (баланс до пополнения+ сумма пополнения + бонус)
-        System.out.println(BalanceAfter + " итоговый баланс");
+        int balanceAfter = balanceBefore+incomingPayment+bonus; // рассчитать итоговый баланс (баланс до пополнения+ сумма пополнения + бонус)
+        System.out.println(balanceAfter + " итоговый баланс");
     }
 }
