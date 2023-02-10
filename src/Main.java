e5479409af2c80f2d3096827f21668f2b3f35350
Diana Basenkova <dinnight123@yandex.ru>
2023-02-10 10:32:20 +0300

    fixed formula

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-
-        int balanceBefore = 85; //баланс до пополнения
-        int incomingPayment = 900; // сумма пополнения
+        // пример 2
+        int balanceBefore = 100; //баланс до пополнения
+        int incomingPayment = 1100; // сумма пополнения
         int payment = 1000; // сумма при превышении которой начисляются бонусы
-        int bonus = incomingPayment>payment ? ((incomingPayment - payment) / 100) : (0); // если платеж >1000, то рассчитать бонус с разницы превышающей суммы;
+        int bonus = incomingPayment > payment ? incomingPayment / 100 : (0); // если платеж >1000, то рассчитать бонус с разницы превышающей суммы;
         System.out.println(bonus + " бонусных рублей");
-        int balanceAfter = balanceBefore+incomingPayment+bonus; // рассчитать итоговый баланс (баланс до пополнения+ сумма пополнения + бонус)
+        int balanceAfter = balanceBefore + incomingPayment + bonus; // рассчитать итоговый баланс (баланс до пополнения+ сумма пополнения + бонус)
         System.out.println(balanceAfter + " итоговый баланс");
+
     }
 }
